44c16ac3fd8d8073da38a1358c1f3c660ba0077a
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-08-24 11:37:34 -0300

    primeiro requisito

---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="name">
+          <input data-testid="name-input" id="name" type="text" />
+        </label>
+        <label htmlFor="description">
+          <input data-testid="description-input" id="description" type="textarea" />
+        </label>
+        <label htmlFor="attr1">
+          <input data-testid="attr1-input" id="attr1" type="number" />
+        </label>
+        <label htmlFor="attr2">
+          <input data-testid="attr2-input" id="attr2" type="number" />
+        </label>
+        <label htmlFor="attr3">
+          <input data-testid="attr3-input" id="attr3" type="number" />
+        </label>
+        <label htmlFor="image">
+          <input data-testid="image-input" id="image" type="text" />
+        </label>
+        <label htmlFor="rare">
+          <select data-testid="rare-input" name="cars" id="rare">
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="trunfo">
+          <input data-testid="trunfo-input" id="trunfo" type="checkbox" />
+        </label>
+        <label htmlFor="save">
+          <input data-testid="save-button" id="save" type="button" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default Form;
